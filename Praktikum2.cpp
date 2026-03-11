4189de40c95f166900f908a7547194ad979037ca
muh arya saputra <aryaom74@gmail.com>
2026-03-11 11:11:29 +0700

    Create Praktikum2.cpp

---
+++ Praktikum2.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+void inputData(float &berat, float &tinggi) {
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan tinggi badan (m): ";
+    cin >> tinggi;
+}
+
+
+float hitungBMI(float berat, float tinggi) {
+    return berat / (tinggi * tinggi);
+}
+
+
+string kondisiBerat(float bmi) {
+    if (bmi < 18.5) {
+        return "Berat Badan Kurang";
+    } else if (bmi < 25) {
+        return "Berat Badan Normal";
+    } else if (bmi < 30) {
+        return "Berat Badan Kelebihan";
+    } else {
+        return "Obesitas";
+    }
+}
+
+int main() {
+    float berat, tinggi, bmi;
+    string kondisi;
+
+    cout << "=== Program Kalkulator BMI ===" << endl;
+    cout << endl;
+
+    
+    inputData(berat, tinggi);
+
+    
+    bmi = hitungBMI(berat, tinggi);
+
+    
+    kondisi = kondisiBerat(bmi);
+
+    cout << endl;
+    cout << "=== Hasil ===" << endl;
+    cout << "BMI Anda: " << bmi << endl;
+    cout << "Kondisi: " << kondisi << endl;
+
+    return 0;
+}
