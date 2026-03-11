0f0fdc057e21d16e82c1aff3d3f1932d6efc8dd9
aryaom74-max <aryaom74@gmail.com>
2026-03-11 11:14:32 +0700

    Update Praktikum2.cpp

--- Praktikum2.cpp
+++ Praktikum2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 
 void inputData(float &berat, float &tinggi) {
-    cout << "Masuk berat badan (kg): ";
+    cout << "Masukkan berat badan (kg): ";
     cin >> berat;
     cout << "Masukkan tinggi badan (m): ";
     cin >> tinggi;
