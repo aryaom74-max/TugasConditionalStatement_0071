89c2d2f98970eeb2ad70b83b8a5d7f2f3bfc05e7
aryaom74-max <aryaom74@gmail.com>
2026-03-11 11:15:14 +0700

    Update Praktikum2.cpp

--- Praktikum2.cpp
+++ Praktikum2.cpp
@@ -19,7 +19,7 @@ float hitungBMI(float berat, float tinggi) {
 string kondisiBerat(float bmi) {
     if (bmi < 18.5) {
         return "Berat Badan Kurang";
-    } else if (bmi < 2) {
+    } else if (bmi < 25) {
         return "Berat Badan Normal";
     } else if (bmi < 30) {
         return "Berat Badan Kelebihan";
